b5d988be5f3fb350818f0592b1fb53cd9b279162
abikem01 <babayod@yahoo.com>
2022-11-25 07:54:11 +0100

    merged

--- src/Components/ItemSlider.js
+++ src/Components/ItemSlider.js
@@ -3,16 +3,20 @@ import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
-function ItemSlider({ children }) {
+function ItemSlider({ children, last }) {
     const settings = {
         arrows: false,
-        infinite: true,
+        infinite: false,
         speed: 500,
         slidesToShow: 6,
-        slidesToScroll: 1,
+        slidesToScroll: JSON.parse(localStorage.getItem('scrollToEnd')) == true ? last : 1,
         autoplay: true,
         autoplaySpeed: 2000,
+        cssEase: "linear",
         vertical: true,
+        verticalSwiping: true,
+        // initialSlide: children - 1,
+
     }
     return <Slider {...settings}>{children}</Slider>
 }
